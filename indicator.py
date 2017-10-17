b57784ad2a661af1bced92424f3894a3b9be8416
basraznov <bas_thehero_123@hotmail.com>
2017-10-17 11:26:48 +0700

    add get last

--- indicator.py
+++ indicator.py
@@ -6,6 +6,13 @@ def flaot2deciamal(data):
 	data = [float(i) for i in data]
 	return data
 
+def getLast(data):
+	newdata=[]
+	for x in range(0,len(data)):
+		newdata.append(data[x][5])
+	return newdata
+
+
 def getData(Symbol):
 	db = MySQLdb.connect(host='127.0.0.1',user='root',passwd='',db='Project')
 	sql = "SELECT * FROM `trade` WHERE Symbol = %s"
@@ -45,20 +52,32 @@ def MACD(data):
 	macd = flaot2deciamal(macd)
 	return macd
 
-# def RSI(day,data):
-# 	GL=[0.0]
-# 	for x in range(1,len(data)):
-# 		tmp = data[x][5]-data[x-1][5]
-# 		GL.append(tmp)
-# 	GL = flaot2deciamal(GL)
-# 	AG = 0.0
-# 	AL = 0.0
-# 	for x in (0,day):
-# 		if x > 0:
-# 			AG = AG + GL[x]
-# 		else:
-# 			AL = AL + GL[x]
-# 	print AL,AG
+def RSI(day,data):
+	GL=[0.0]
+	print data[0][5],
+	for x in range(1,len(data)):
+		print data[x][5],
+		tmp = data[x][5]-data[x-1][5]
+		GL.append(tmp)
+	GL = flaot2deciamal(GL)
+	AG = 0.0
+	AL = 0.0
+	for x in range(0,day):
+		if GL[x] > 0.0:
+			AG = AG + GL[x]
+		else:
+			AL = AL + GL[x]
+	AG = AG/day
+	AL = AL/day
+	RS = AG/AL
+	print AG,AL,RS
+	RSI = [0.0]
+	RSI[0] = 100-(100/(1+RS))
+	print RSI[0]
+
+
 
 data = getData("AAV")
-RSI(day=14,data = data)
+print getLast(data)
+# print EMA(5,[11,12,13,14,15,16,17])
+# RSI(day=14,data = data)
