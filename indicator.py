f5325e9abd73f1ea7c37fe340e6d10c25579cb4e
basraznov <bas_thehero_123@hotmail.com>
2017-10-27 23:27:38 +0700

    change indicator

--- indicator.py
+++ indicator.py
@@ -1,27 +1,5 @@
 import pymysql
-
-
-def flaot2deciamal(data):
-	data = [ '%.3f' % elem for elem in data ]
-	data = [float(i) for i in data]
-	return data
-
-def getLast(data):
-	data = list(data)
-	newdata=[]
-	for x in range(0,len(data)):
-		newdata.append(data[x][5])
-	return newdata
-
-
-def getData(Symbol):
-	db = pymysql.connect(host='127.0.0.1',user='root',passwd='',db='Project')
-	sql = "SELECT * FROM `trade` WHERE Symbol = %s"
-	data=[Symbol]
-	cursor = db.cursor()
-	cursor.execute(sql,data)
-	results = cursor.fetchall()
-	return results
+from getandformat import *
 
 def EMA(day,data):
 	data = list(data)
