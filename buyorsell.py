d01f3b6a332f2ad9d6cececddcf48cfc1b6d8041
basraznov <bas_thehero_123@hotmail.com>
2017-11-21 18:02:59 +0700

    Updata Database

--- buyorsell.py
+++ buyorsell.py
@@ -19,7 +19,7 @@ from getandformat import *
 
 
 def buy(pLast,nLast,macd,rsi,avgVol,vol):
-	print(pLast,nLast,macd,rsi,avgVol,vol)
+	# print(pLast,nLast,macd,rsi,avgVol,vol)
 	if(nLast >= pLast):
 		if(vol > avgVol):
 			if(rsi > 30 and rsi < 70):
@@ -33,15 +33,18 @@ def sell(pLast,nLast,avgVol,vol,ema):
 		if(nLast < ema):
 			return True
 	return False
-			
 
-# macd = 0.007
-# rsi = 45.791
-# pLast = 6.4
-# nLast = 6.4
-# vol = 12335927
-# avgVol = 40013446.6834
-# ema5 = 6.42
+macd = 0.007
+rsi = 45.791
+pLast = 6.4
+nLast = 6.4
+vol = 12335927
+avgVol = 40013446.6834
+ema5 = 6.42
 
-# print(buy(pLast=pLast,nLast=nLast,macd=macd,rsi=rsi,avgVol=avgVol,vol=vol))
-# print(sell(pLast=pLast,nLast=nLast,avgVol=avgVol,vol=vol,ema=ema5))
+if buy(pLast=pLast,nLast=nLast,macd=macd,rsi=rsi,avgVol=avgVol,vol=vol):
+	print("Buy")
+elif sell(pLast=pLast,nLast=nLast,avgVol=avgVol,vol=vol,ema=ema5):
+	print("Sell")
+else:
+	print("Nothing")
