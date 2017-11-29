cf4a32ba13555e6d6807472a789151dd991d6f0c
basraznov <bas_thehero_123@hotmail.com>
2017-11-29 16:15:00 +0700

    Updata csvtosql

--- csvtosql.py
+++ csvtosql.py
@@ -3,16 +3,20 @@ import pymysql
 
 mydb = pymysql.connect(host='127.0.0.1',user='root',passwd='',db='Project')
 cursor = mydb.cursor()
-csv_data = csv.reader(file('Trade.csv'))
+csv_data = csv.reader(open('Trade.csv'))
 i = 0
 for row in csv_data:
-	k = '20'+row[0][6]+row[0][7]+'-'+row[0][3]+row[0][4]+'-'+row[0][0]+row[0][1]
+	# k = '20'+row[0][6]+row[0][7]+'-'+row[0][3]+row[0][4]+'-'+row[0][0]+row[0][1]
 	for i in range(0,len(row)):
 		if row[i] == '':
 			row[i] = None
-	data = [k,row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8]]
+		if row[i] == '-':
+			row[i] = None
+	if row[10] != '-' and row[10] != '' and row[10] != None or row[11] != '-' and row[11] != '' and row[11] != None:
+		row[10] = row[10].replace(',', '')
+	data = ['2017-11-27',row[0],row[2],row[3],row[4],row[5],row[7],row[10],row[11]]
 	sql = 'INSERT INTO trade(`Date`,`Symbol`,`Open`,`High`,`Low`,`Last`,`ChPer`,`Volumn`,`Money`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)'
 	cursor.execute(sql,data)
 	mydb.commit()
 cursor.close()
-print "Done"
+print ("Done")
