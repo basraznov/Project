efe204b82075709adf858f869ca7d58a25b1d8de
basraznov <bas_thehero_123@hotmail.com>
2017-12-25 18:03:05 +0700

    Updata Database 2017-12-22

--- csvtosql.py
+++ csvtosql.py
@@ -3,7 +3,7 @@ import pymysql
 
 mydb = pymysql.connect(host='127.0.0.1',user='root',passwd='',db='Project')
 cursor = mydb.cursor()
-csv_data = csv.reader(open('Trade.csv'))
+csv_data = csv.reader(open('../18122017.csv'))
 i = 0
 for row in csv_data:
 	# k = '20'+row[0][6]+row[0][7]+'-'+row[0][3]+row[0][4]+'-'+row[0][0]+row[0][1]
@@ -14,9 +14,11 @@ for row in csv_data:
 			row[i] = None
 	if row[10] != '-' and row[10] != '' and row[10] != None or row[11] != '-' and row[11] != '' and row[11] != None:
 		row[10] = row[10].replace(',', '')
-	data = ['2017-11-27',row[0],row[2],row[3],row[4],row[5],row[7],row[10],row[11]]
+	data = ['2017-12-18',row[0],row[2],row[3],row[4],row[5],row[7],row[10],row[11]]
+	# print(data)
 	sql = 'INSERT INTO trade(`Date`,`Symbol`,`Open`,`High`,`Low`,`Last`,`ChPer`,`Volumn`,`Money`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)'
 	cursor.execute(sql,data)
 	mydb.commit()
 cursor.close()
+
 print ("Done")
