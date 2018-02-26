a984e87cc5c1019f639ccb296930216bd47816bb
basraznov <bas_thehero_123@hotmail.com>
2018-02-26 17:44:37 +0700

    Update Database 2018-02-22

--- csvtosql.py
+++ csvtosql.py
@@ -3,10 +3,12 @@ import pymysql
 
 mydb = pymysql.connect(host='127.0.0.1',user='root',passwd='',db='Project')
 cursor = mydb.cursor()
-csv_data = csv.reader(open('../09022018.csv'))
+csv_data = csv.reader(open('../22022018.csv'))
 i = 0
 for row in csv_data:
 	# k = '20'+row[0][6]+row[0][7]+'-'+row[0][3]+row[0][4]+'-'+row[0][0]+row[0][1]
+	# for x in range(0,len(row)):
+	# 	print(x,row[x])
 	for i in range(0,len(row)):
 		if row[i] == '':
 			row[i] = None
@@ -14,7 +16,7 @@ for row in csv_data:
 			row[i] = None
 	if row[10] != '-' and row[10] != '' and row[10] != None or row[11] != '-' and row[11] != '' and row[11] != None:
 		row[10] = row[10].replace(',', '')
-	data = ['2018-02-09',row[0],row[2],row[3],row[4],row[5],row[7],row[10],row[11]]
+	data = ['2018-02-22',row[0],row[2],row[3],row[4],row[5],row[7],row[10],row[11]]
 	if row[0] == None:
 		continue
 	print(data)
