b8e28e1c17f0fa99525bc3ef0a2c84469117e722
basraznov <bas_thehero_123@hotmail.com>
2018-04-07 18:35:33 +0700

    Update showfav

--- api/showfav.php
+++ api/showfav.php
@@ -1,21 +1,23 @@
 <?php 
     include("database.php");
+    include("lastupdate.php");
     header('Content-Type: application/json');
     session_start();
     if(isset($_SESSION['username'])){
+        $last_update = $date;
         $username = $_SESSION['username'];
-        $stmt = $db->prepare("SELECT * FROM `favorite` WHERE username = ?;");
-        $stmt->bind_param('s',$username);
+        $stmt = $db->prepare("SELECT * FROM `trade` WHERE Symbol in (SELECT stock FROM `favorite` WHERE username = ?) and Date = ?;");
+        $stmt->bind_param('ss',$username,$date);
         $stmt->execute();
-        $k = "[";
+        $k = "";
+        $x = 0;
         $result = mysqli_stmt_get_result($stmt);
         while ($row = mysqli_fetch_array($result, MYSQLI_NUM)){
-            // $k = $k.'"'.$row[1].'",';
-            echo $row[1];
+            $k = '"'.$x.'":["'.$row[1].'","'.$row[2].'","'.$row[5].'","'.$row[6].'"],'.$k;
+            $x++;
         }
-        die();
         $k = rtrim($k,",");
-        echo '{"status":"Success","stock":'.$k.']}';
+        echo '{"status":"Success",'.$k.'}';
     }
     else{
         echo '{"status":"Require login or wrong parameter"}';
