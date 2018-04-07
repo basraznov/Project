8629c5071f9a07ea4d8836b9942001cdcfd4a45f
basraznov <bas_thehero_123@hotmail.com>
2018-04-07 19:27:59 +0700

    Update showfav

--- api/showfav.php
+++ api/showfav.php
@@ -3,6 +3,16 @@
     include("lastupdate.php");
     header('Content-Type: application/json');
     session_start();
+
+    function searchForId($id, $array) {
+        foreach ($array as $key => $val) {
+            if ($val[0] === $id) {
+                return $val[1];
+            }
+        }
+        return false;
+    }
+
     if(isset($_SESSION['username'])){
         $last_update = $date;
         $username = $_SESSION['username'];
@@ -12,8 +22,32 @@
         $k = "";
         $x = 0;
         $result = mysqli_stmt_get_result($stmt);
+        $myfile = fopen("../main/PD.txt", "r") or die("Unable to open file!");
+        $file = fread($myfile,filesize("../main/PD.txt"));
+        $trend = "Hold";
+        if($file === "None"){
+            $trend = "Hold";
+        }
+        else{
+            $file = explode("], [",$file);
+            foreach ($file as &$value) {
+                $value = explode(", ",$value);
+                foreach ($value as &$supva){
+                    $supva = preg_replace('/[^a-z0-9\-]/i', '', $supva);
+                }
+            }
+        }
         while ($row = mysqli_fetch_array($result, MYSQLI_NUM)){
-            $k = '"'.$x.'":["'.$row[1].'","'.$row[2].'","'.$row[5].'","'.$row[6].'"],'.$k;
+            if($file === "None"){
+                $trend = "Hold";
+            }
+            else{
+                $trend = searchForId($row[1],$file);
+                if (!$trend){
+                    $trend = "Hold";
+                }
+            }
+            $k = '"'.$x.'":["'.$row[1].'","'.$row[2].'","'.$row[5].'","'.$row[6].'","'.$trend.'"],'.$k;
             $x++;
         }
         $k = rtrim($k,",");
